1651ade0747d3363a13d44bde861168d3fc1b655
darkhouseaudio <darkhouseaudio@gmail.com>
2025-05-05 23:06:31 -0600

    Automatic update 05/05/2025 23:06:31,15

--- commit-purchases.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Running purchase database commit script...
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0commit-purchases.ps1" %*
-pause 
--- push-updates.bat
+++ push-updates.bat
@@ -1,17 +1,7 @@
 @echo off
 echo Pushing all updates to GitHub...
-
-:: First, add all standard files
 git add .
-
-:: Force add the webhook and workflows directories
-git add webhook-handler/ -f
-git add .github/workflows/ -f  
-
-:: Commit all changes
 git commit -m "Automatic update %date% %time%"
-
-:: Push to GitHub
 git push
-
 echo Done!
+pause 
